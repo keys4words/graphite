1ea00cd8fc0baaa0381384ef822bb37bbd027237
keys4words <keys4words@gmail.com>
2022-06-18 23:40:48 +0300

    remove doubles

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-VERSION=1.1.10-1
-docker build . \
-  --build-arg python_extra_flags="--single-version-externally-managed --root=/" \
-  --no-cache --tag graphiteapp/graphite-statsd:$VERSION --progress tty
